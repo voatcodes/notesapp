cf467ea972eb687a9df2b08d017c60cf53a74658
voatcodes <valarietila@gmail.com>
2022-08-22 19:16:11 +0300

    configured access form information,creates a create-note and save note route

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+//config to access form information
+app.use(express.urlencoded({extended:false}))
+
 app.get('/', (req, res) => {
     let title = 'Index page'
     res.render('index', {title: title})
@@ -10,6 +13,24 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     let title = 'About page'
     res.render('about', {title: title})
+})
+
+/* create-note */
+
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+//save a note
+
+app.post('/create', (req, res) =>{
+    const note = {
+        title:req.body.title,
+        body:req.body.body
+    }
+
+    console.log(note)
+
 })
 
 
