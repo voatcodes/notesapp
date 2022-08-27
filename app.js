26083af2370cd589f5cb08cec4c9f6fdceedcf1e
voatcodes <valarietila@gmail.com>
2022-08-27 12:47:48 +0300

    created edit note and delete note routes

--- app.js
+++ app.js
@@ -61,8 +61,68 @@ app.post('/create', (req, res) =>{
 
 
 
+//view a single note
+
+app.get('/note/:id', (req, res) => {
+    let sql = 'SELECT * FROM note WHERE id=?'
+    connection.query(
+        sql, [parseInt(req.params.id)],
+         (error, results) => {
+            res.render('note', {note: results[0]})
+         }
+    )
+})
+
+ //edit note
+app.get('/edit/:id', (req, res) => {
+    let sql = 'SELECT * FROM note WHERE id=?'
+    connection.query(
+        sql, [parseInt(req.params.id)],
+        (error, results) => {
+            res.render('edit', {note: results[0]})
+        }
+    )
+
+})
+
+
+app.post('/edit/:id', (req, res) => {
+    const note = {
+        title: req.body.title,
+        body: req.body.body
+    }
+    let sql = 'UPDATE note SET title=?, body=? WHERE id=?'
+    connection.query(
+        sql, [note.title, note.body, parseInt(req.params.id)],
+        (error, results) =>{
+            res.redirect('/')
+        }
+    )
+})
+
+
+
+
+ // delete note
+
+ app.get('/delete/:id', (req, res) => {
+    let sql = 'DELETE FROM note WHERE id=?'
+    connection.query(
+        sql, [parseInt(req.params.id)],
+        (error, results) => {
+            if (error) {
+                console.log(error)
+            } else {
+                res.redirect('/')   
+            }
+            
+        }
+    )
+ })
+
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`app is live on PORT ${PORT}...`)
 })
+
